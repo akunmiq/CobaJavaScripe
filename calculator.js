85c052d89150f6d53ca5fb154ff30b597f480332
akunmiq <miqdad.saja23@gmail.com>
2018-09-03 17:07:56 +0700

    cobajavascripe

---
+++ calculator.js
@@ -0,0 +1,33 @@
+//Penjumlahan
+var angka1 = 4;
+var angka2 = 5;
+
+console.log(angka2 + angka1); // 9
+
+
+//Pengurangan
+var angka3 = 8;
+var angka4 = 3;
+
+console.log(angka3 - angka4); // 5
+
+
+//Perkalian
+var angka5 = 7;
+var angka6 = 2;
+
+console.log(angka5 * angka6); // 14
+
+
+//Pembagian
+var angka7 = 6;
+var angka8 = 1;
+
+console.log(angka7 / angka8); // 6
+
+
+//Modulus
+var angka9 = 9;
+var angka10 = 2;
+
+console.log(angka9 % angka10); // 1
