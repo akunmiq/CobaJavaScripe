a57bfec2771d805935bb29955cbf9b2c17077797
akunmiq <miqdad.saja23@gmail.com>
2018-09-04 11:15:10 +0700

    cobajavascript

---
+++ rumusbangundatar.js
@@ -0,0 +1,39 @@
+// luas persegi
+var a = prompt("masukan nilai panjang");
+var b = prompt("masukan nilai lebar");
+var luas = a * b;
+
+console.log("luas persegi panjang = "+luas+" cm2");
+
+
+// luas segitiga
+var c = prompt("masukan nilai alas");
+var d = prompt("masukan nilai tinggi");
+var luas = 0.5 * c * d;
+
+console.log("luas segitiga = "+luas+" cm2");
+
+
+// luas jajar genjang
+var e = prompt("masukan nilai alas");
+var f = prompt("masukan nilai tinggi");
+var luas = e * f
+
+console.log("luas jajar genjang = "+luas+" cm2");
+
+
+// volume kubus
+var g = prompt("masukan nilai sisi");
+var volume = g * g * g;
+
+console.log("volume kubus = "+volume+" cm3");
+
+
+// volume tabung
+var Pi = (22 / 7);
+var h = prompt("masukan nilai jari-jari");
+var i = prompt("masukan nilai tinggi");
+
+var volume = Pi * h * h * i;
+
+console.log("volume tabung = "+volume+" cm3");
